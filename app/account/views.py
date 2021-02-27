ca44079cc4cd8c96f852702a78a958bc616370a7
Paul C. Owe <40435982+paulowe@users.noreply.github.com>
2021-02-28 02:29:27 +0300

    minor fix before merging to master

--- app/account/views.py
+++ app/account/views.py
@@ -266,8 +266,8 @@ def join_from_invite(user_id, token):
 @account.before_app_request
 def before_request():
     """Force user to confirm email before accessing login-required routes."""
-    if current_user.is_authenticated \
-            and current_user.confirmed \
+    if current_user.is_authenticated() \
+            and not current_user.confirmed \
             and request.endpoint[:8] != 'account.' \
             and request.endpoint != 'static':
         return redirect(url_for('account.unconfirmed'))
